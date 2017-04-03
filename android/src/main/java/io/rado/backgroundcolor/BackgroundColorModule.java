b496cafc6f132a8a4640ebb7fda8a07138cb6ae5
radoslav.milushev <radoslav.milushev@trading212.com>
2017-04-03 13:27:02 +0300

    Fix module class name

--- android/src/main/java/io/rado/backgroundcolor/BackgroundColorModule.java
+++ android/src/main/java/io/rado/backgroundcolor/BackgroundColorModule.java
@@ -18,7 +18,7 @@ public class BackgroundColorModule extends ReactContextBaseJavaModule {
   private static final String DURATION_SHORT_KEY = "SHORT";
   private static final String DURATION_LONG_KEY = "LONG";
 
-  public Module(ReactApplicationContext reactContext) {
+  public BackgoundColorModule(ReactApplicationContext reactContext) {
     super(reactContext);
   }
 
